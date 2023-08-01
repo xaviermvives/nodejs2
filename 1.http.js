2db08bb337662aae0bb06429519579df43f28e59
xavier <montjuic79@gmail.com>
2023-08-01 18:50:43 +0200

    First routes and charset problem

--- 1.http.js
+++ 1.http.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 const http = require('node:http')
 
 console.log(process.env)
@@ -5,8 +6,18 @@ console.log(process.env)
 const desiredPort = process.env.PORT ?? 3000
 
 const processRequest = (req, res) => {
-    console.log('request received:', req.url)
-    res.end('hola mundo')
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+
+    if (req.url === '/') {
+        res.statusCode = 200
+        res.end('<h1>Bienvenido a mi página web</h1>')
+    } else if (req.url === '/contacto') {
+        res.statusCode = 200
+        res.end('<h1>Contacto</h1>')
+    } else {
+        res.statusCode = 404 // Not found
+        res.end('<h1>404</h1>')
+    }
 }
 
 const server = http.createServer(processRequest)
