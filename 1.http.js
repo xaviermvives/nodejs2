f10c6a3dca3572e8bd28b51af34c378b915ccf59
xavier <montjuic79@gmail.com>
2023-08-01 18:16:32 +0200

    Basic http server

---
+++ 1.http.js
@@ -0,0 +1,14 @@
+const http = require('node:http')
+
+console.log(process.env)
+
+const desiredPort = process.env.PORT ?? 3000
+
+const server = http.createServer((req, res) => {
+    console.log('request received')
+    res.end('hola mundo')
+})
+
+server.listen(desiredPort, () => {
+    console.log(`server rlistenning on port http://localhost${desiredPort}`)
+})
