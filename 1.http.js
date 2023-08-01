afe6a0ee4c23b455ccc8eb7d6970b690eeecc674
xavier <montjuic79@gmail.com>
2023-08-01 18:37:28 +0200

    Extracting the function processRequest()

--- 1.http.js
+++ 1.http.js
@@ -4,10 +4,12 @@ console.log(process.env)
 
 const desiredPort = process.env.PORT ?? 3000
 
-const server = http.createServer((req, res) => {
+const processRequest = (req, res) => {
     console.log('request received:', req.url)
     res.end('hola mundo')
-})
+}
+
+const server = http.createServer(processRequest)
 
 server.listen(desiredPort, () => {
     console.log(`server rlistenning on port http://localhost${desiredPort}`)
