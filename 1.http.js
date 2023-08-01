f9458d55ad76f949281af8b0b6e933c53f170c1f
xavier <montjuic79@gmail.com>
2023-08-01 19:28:50 +0200

    Serving a static file (image)

--- 1.http.js
+++ 1.http.js
@@ -1,5 +1,6 @@
 /* eslint-disable indent */
 const http = require('node:http')
+const fs = require('node:fs')
 
 console.log(process.env)
 
@@ -14,6 +15,17 @@ const processRequest = (req, res) => {
     } else if (req.url === '/contacto') {
         res.statusCode = 200
         res.end('<h1>Contacto</h1>')
+    } else if (req.url === '/image.png') {
+        fs.readFile('./bitcoin.png', (err, data) => {
+            if (err) {
+                res.statusCode = 500
+                res.end('<h1>Internal Server Error</h1>')
+            } else {
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'image/png')
+                res.end(data)
+            }
+        })
     } else {
         res.statusCode = 404 // Not found
         res.end('<h1>404</h1>')
