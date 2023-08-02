09d71d88852da94f2067832eac716d7dd2609b4a
xavier <montjuic79@gmail.com>
2023-08-02 19:00:29 +0200

    GET modified

--- 3.express.js
+++ 3.express.js
@@ -1,16 +1,17 @@
 /* eslint-disable indent */
 const express = require('express')
-
-const app = express()
-
-app.disable('x-powered-by')
+const ditto = require('./pokemon/ditto.json')
 
 const PORT = process.env.PORT ?? 3005
 
-app.get('/', (req, res) => {
+const app = express()
+app.disable('x-powered-by')
+
+app.get('/pokemon/ditto', (req, res) => {
     // res.status(200).send('<h1>Mi página</h1>')
     // eslint-disable-next-line indent
-    res.json({ message: 'Hola mundo' })
+    // res.json({ message: 'Hola mundo' })
+    res.json(ditto)
 })
 
 app.post('/pokemon', (req, res) => {
@@ -27,6 +28,11 @@ app.post('/pokemon', (req, res) => {
         data.timestamp = Date.now()
         res.status(201).json(data)
     })
+})
+
+// la última a la que va a pasar
+app.use((req, res) => {
+    res.status(404).send('<h1>404</h4>')
 })
 
 app.listen(PORT, () => {
