a9329360ab164a5f33d3c710a8c4db5a1133992f
xavier <montjuic79@gmail.com>
2023-08-02 17:53:23 +0200

    POST request

--- 2.routing.js
+++ 2.routing.js
@@ -18,6 +18,31 @@ const processRequest = (req, res) => {
                     res.setHeader('Content-Type', 'text/html; charset=utf-8')
                     return res.end('<h1>404</h1>')
             }
+        case 'POST':
+            switch (url) {
+                case '/pokemon': {
+                    let body = ''
+
+                    // escuchar evento data
+                    req.on('data', chunk => {
+                        body += chunk.toString()
+                    })
+
+                    req.on('end', () => {
+                        const data = JSON.parse(body)
+                        // llamar a una base de datos par guardar la info
+                        res.writeHead(201, { 'Content-Type': 'application/json; charset=utf-8' })
+                        data.timestamp = Date.now()
+                        res.end(JSON.stringify(data))
+                    })
+                    break
+                }
+
+                default:
+                    res.statusCode = 404
+                    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+                    return res.end('404 Not Found')
+            }
     }
 }
 
